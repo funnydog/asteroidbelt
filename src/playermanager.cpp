f8c27154e1f90c8132b9eae5a4b232dd84f5c3a2
funnydog <funnydog@users.noreply.github.com>
2023-09-20 08:54:28 +0200

    Add the PlayerManager class to handle the player.

---
+++ src/playermanager.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+
+#include <GLFW/glfw3.h>
+
+#include "playermanager.hpp"
+
+#include "window.hpp"
+
+namespace
+{
+}
+
+PlayerManager::PlayerManager(
+	const FloatRect &screenBounds,
+	const Texture &texture, const FloatRect &initialFrame,
+	unsigned frameCount)
+	: mPlayerSprite(texture, initialFrame, {500, 500}, {})
+	, mPlayerSpeed(160.f)
+	, mPlayerAreaLimit{
+			{0.f, screenBounds.size.y * 0.5f},
+			{screenBounds.size.x, screenBounds.size.y * 0.5f}
+		}
+	, mPlayerScore(0)
+	, mLivesRemaining(3)
+	, mDestroyed(false)
+	, mGunOffset(45.f * 0.5f, 10.f)
+	, mShotTimer(0.f)
+	, mMinShotTimer(0.2f)
+	, mPlayerRadius(15.f)
+	, mShotManager(screenBounds, 250.f, 2, texture, {{0.f, 300.f}, {5.f, 5.f}}, 4)
+{
+	FloatRect frame = initialFrame;
+	for (unsigned i = 1; i < frameCount; i++)
+	{
+		frame.pos.x += frame.size.x;
+		mPlayerSprite.addFrame(frame);
+	}
+	mPlayerSprite.setFrameDelay(0.0167f);
+	mPlayerSprite.setCollisionRadius(mPlayerRadius);
+}
+
+void
+PlayerManager::fireShot()
+{
+	if (mShotTimer >= mMinShotTimer)
+	{
+		mShotTimer = 0.f;
+		mShotManager.fireShot(
+			mPlayerSprite.getLocation() + mGunOffset,
+			glm::vec2(0.f, -1.f),
+			true);
+	}
+}
+
+void
+PlayerManager::handleKeyboardInput(Window &window)
+{
+	int h = window.isKeyPressed(GLFW_KEY_RIGHT) - window.isKeyPressed(GLFW_KEY_LEFT);
+	int v = window.isKeyPressed(GLFW_KEY_DOWN) - window.isKeyPressed(GLFW_KEY_UP);
+	auto vel = mPlayerSprite.getVelocity() + glm::vec2(h, v);
+	if (vel.x != 0.f && vel.y != 0.f)
+	{
+		vel = glm::normalize(vel);
+	}
+	mPlayerSprite.setVelocity(vel);
+
+	if (window.isKeyPressed(GLFW_KEY_SPACE))
+	{
+		fireShot();
+	}
+}
+
+void
+PlayerManager::handleGamepadInput()
+{
+	// TODO
+}
+
+void
+PlayerManager::limitMovements()
+{
+	auto dst = mPlayerSprite.getDestination();
+	glm::vec2 loc;
+	loc.x = std::clamp(
+		dst.pos.x,
+		mPlayerAreaLimit.pos.x,
+		mPlayerAreaLimit.pos.x + mPlayerAreaLimit.size.x - dst.size.x);
+	loc.y = std::clamp(
+		dst.pos.y,
+		mPlayerAreaLimit.pos.y,
+		mPlayerAreaLimit.pos.y + mPlayerAreaLimit.size.y - dst.size.y);
+
+	 mPlayerSprite.setLocation(loc);
+}
+
+void
+PlayerManager::update(float dt, Window &window)
+{
+	mShotManager.update(dt);
+	if (!mDestroyed)
+	{
+		mPlayerSprite.setVelocity(glm::vec2(0.f));
+
+		mShotTimer += dt;
+		handleKeyboardInput(window);
+		handleGamepadInput();
+
+		mPlayerSprite.setVelocity(
+			mPlayerSprite.getVelocity() * mPlayerSpeed);
+
+		mPlayerSprite.update(dt);
+		limitMovements();
+	}
+}
+
+void
+PlayerManager::draw(RenderTarget &target)
+{
+	mShotManager.draw(target);
+	if (!mDestroyed)
+	{
+		mPlayerSprite.draw(target);
+	}
+}
---
+++ src/playermanager.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+#include "rect.hpp"
+#include "sprite.hpp"
+#include "shotmanager.hpp"
+#include "texture.hpp"
+
+class Window;
+
+class PlayerManager
+{
+public:
+	PlayerManager(const FloatRect &screenBounds,
+		      const Texture &texture, const FloatRect &initialFrame, unsigned frameCount);
+
+	void update(float dt, Window &window);
+	void draw(RenderTarget &target);
+
+private:
+	void fireShot();
+	void handleKeyboardInput(Window &window);
+	void handleGamepadInput();
+	void limitMovements();
+
+private:
+	Sprite mPlayerSprite;
+	float  mPlayerSpeed;
+	FloatRect mPlayerAreaLimit;
+
+	long mPlayerScore;
+	int mLivesRemaining;
+	bool mDestroyed;
+
+	glm::vec2 mGunOffset;
+	float mShotTimer;
+	float mMinShotTimer;
+	float mPlayerRadius;
+
+	ShotManager mShotManager;
+};

fbd76225f3b2a0902054566f04feba26c633b8a4
funnydog <funnydog@users.noreply.github.com>
2023-09-24 14:06:29 +0200

    Add the include path for flycheck-clang.

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,4 +2,4 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((c++-mode . ((flycheck-clang-language-standard . "c++17")
-	      (flycheck-clang-include-path . ("." "/usr/include/SDL2/" "/usr/include/freetype2")))))
+	      (flycheck-clang-include-path . ("." "/usr/include/SDL2/" "/usr/include/freetype2" "/usr/include/AL")))))
